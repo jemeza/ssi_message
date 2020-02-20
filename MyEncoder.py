40a4c66c1a203b41ed82b97b5619075c499b29dd
jemeza <57341979+jemeza@users.noreply.github.com>
2020-02-20 00:43:52 -0800

    Delete MyEncoder.py

--- MyEncoder.py
+++
@@ -1,9 +0,0 @@
-from json import JSONEncoder
-
-class MyEncoder(JSONEncoder):
-    def __init__(self, label, time, data, origin, number):
-        self.label = label
-        self.time = time
-        self.values = data
-        self.origin = origin
-        self.origin = number
